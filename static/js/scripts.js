8e6c38418bb280c79664362994ae8f5b398043c6
Tom Hanley <thanley11@gmail.com>
2014-07-16 13:50:39 +0000

    Added static folder and templates

---
+++ static/js/scripts.js
@@ -0,0 +1,48 @@
+$(function() {
+
+    $(".player11").sortable({ 
+//       #QBs, #RBs, #WRs, #TEs, #DEFs, #Ks
+        connectWith: ".sortable",
+        update: function(event, ui){                                             
+            var qb_order = $("#QBs").sortable('toArray',{attribute:'data-pid'});
+            var rb_order = $("#RBs").sortable('toArray',{attribute:'data-pid'});
+            console.log(qb_order, rb_order);                                                                                                                         
+            $.ajax({
+                type: "POST",
+                url: $SCRIPT_ROOT + "/sort/",
+                contentType: "application/json; charset=utf-8",
+                // this was causing problems reading the variable items and returning undefined values
+                data: {'pid' : qb_order+','+rb_order},
+                // []=2[]=1
+                success: function(data) {
+                    console.log(data);
+                },
+                dataType: "json",
+               });                                            
+        }                                                                        
+     });
+
+    $("#QBs").sortable({                                                    
+        connectWith: ".connectedSortable",
+
+        update: function(event, ui){                                             
+           var serial = $('#QBs').sortable('serialize');                                                                                                                         
+            $.ajax({
+                type: "POST",
+                url: $SCRIPT_ROOT + "/sort/",
+                data: {'content': serial, 'csrfmiddlewaretoken': '{{csrf_token}}'} 
+                });   
+            },                                                                        
+          });
+
+    $("#RBs").sortable({
+        connectWith: ".connectedSortable",
+    });
+
+    $(".player11").click(function(){
+        $(this).toggleClass('highlighted');
+    });
+
+
+
+    });
