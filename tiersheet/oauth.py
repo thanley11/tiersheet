c726274a7610d5057e74b505d5ab39929dae1df1
Tom Hanley <thanley11@gmail.com>
2014-07-18 18:29:59 +0000

    Added oauth and json parsing for player population script

--- tiersheet/oauth.py
+++ tiersheet/oauth.py
@@ -1,5 +1,6 @@
 import os
 from requests_oauthlib import OAuth1Session
+import json
 
 key = os.environ['YAHOO_CONSUMER_KEY']
 secret = os.environ['YAHOO_SECRET_KEY']
@@ -28,4 +29,15 @@ access_token_data = yahoo.fetch_access_token(access_token_url)
 print '\n\nYour access token data:', access_token_data
 
 # Fetch a games as an example
-print '\n\nExample: All the games the user has competed in:', yahoo.get('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games?format=json').json()
+#print '\n\nExample: All the games the user has competed in:', yahoo.get('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games?format=json').json()
+#Gets all QBs from this season
+#print '\n\nExample: All the games the user has competed in:', yahoo.get('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/players;position=QB?format=json').json()
+qbs = yahoo.get('http://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/players;position=QB?format=json').json()
+#print json.dumps(qbs, sort_keys=True,indent=4, separators=(',', ': '))
+#data = json.loads(qbs)
+#print qbs["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["players"]["0"]["player"]
+#print qbs["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["players"]["0"]["player"][0][2]["name"]["full"]
+myResult = qbs["fantasy_content"]["users"]["0"]["user"][1]["games"]["0"]["game"][1]["players"]
+for x in myResult:
+    print x, 'corresponds to', myResult[x]["player"][0][2]["name"]["full"]
+
