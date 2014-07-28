ded8d52c9c806efcc00f89410a83d125240bcbd7
Tom Hanley <thanley11@gmail.com>
2014-07-28 17:02:25 +0000

    Fixed jquery to allow blackout on click - need to fix to ignore blackout on info button click

--- tiersheet/static/js/scripts.js
+++ tiersheet/static/js/scripts.js
@@ -1,48 +1,5 @@
 $(function() {
-
-    $(".player11").sortable({ 
-//       #QBs, #RBs, #WRs, #TEs, #DEFs, #Ks
-        connectWith: ".sortable",
-        update: function(event, ui){                                             
-            var qb_order = $("#QBs").sortable('toArray',{attribute:'data-pid'});
-            var rb_order = $("#RBs").sortable('toArray',{attribute:'data-pid'});
-            console.log(qb_order, rb_order);                                                                                                                         
-            $.ajax({
-                type: "POST",
-                url: $SCRIPT_ROOT + "/sort/",
-                contentType: "application/json; charset=utf-8",
-                // this was causing problems reading the variable items and returning undefined values
-                data: {'pid' : qb_order+','+rb_order},
-                // []=2[]=1
-                success: function(data) {
-                    console.log(data);
-                },
-                dataType: "json",
-               });                                            
-        }                                                                        
-     });
-
-    $("#xQBs").sortable({                                                    
-        connectWith: ".connectedSortable",
-
-        update: function(event, ui){                                             
-           var serial = $('#QBs').sortable('serialize');                                                                                                                         
-            $.ajax({
-                type: "POST",
-                url: "/", 
-                data: {'content': serial, 'csrfmiddlewaretoken': '{{csrf_token}}'} 
-                });   
-            },                                                                        
-          });
-
-    $("#xRBs").sortable({
-        connectWith: ".connectedSortable",
-    });
-
-    $(".player11").click(function(){
+     $(".highlight").click(function(){
         $(this).toggleClass('highlighted');
     });
-
-
-
-    });
+ });
