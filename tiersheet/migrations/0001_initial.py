c43e2747f5824b7d7015373e1351870b8a35dee0
thanley11 <thanley11@gmail.com>
2014-08-09 12:47:15 -0400

    Reset migrations

--- tiersheet/migrations/0001_initial.py
+++ tiersheet/migrations/0001_initial.py
@@ -13,10 +13,10 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=64)),
             ('position', self.gf('django.db.models.fields.CharField')(max_length=3)),
-            ('bye', self.gf('django.db.models.fields.IntegerField')()),
-            ('url', self.gf('django.db.models.fields.URLField')(default=None, max_length=200, blank=True)),
+            ('bye', self.gf('django.db.models.fields.IntegerField')(null=True)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
             ('order', self.gf('django.db.models.fields.IntegerField')(default=0)),
-            ('team', self.gf('django.db.models.fields.CharField')(default=None, max_length=3, blank=True)),
+            ('team', self.gf('django.db.models.fields.CharField')(default=None, max_length=3, null=True, blank=True)),
         ))
         db.send_create_signal(u'tiersheet', ['Player'])
 
@@ -29,13 +29,13 @@ class Migration(SchemaMigration):
     models = {
         u'tiersheet.player': {
             'Meta': {'ordering': "['order']", 'object_name': 'Player'},
-            'bye': ('django.db.models.fields.IntegerField', [], {}),
+            'bye': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'}),
             'order': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'position': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
-            'team': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '3', 'blank': 'True'}),
-            'url': ('django.db.models.fields.URLField', [], {'default': 'None', 'max_length': '200', 'blank': 'True'})
+            'team': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '3', 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
         }
     }
 
