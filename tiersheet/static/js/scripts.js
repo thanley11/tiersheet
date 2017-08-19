2f73f824596b64c7fb612cd498c3bc024345ad53
Tom Hanley <thanley11@gmail.com>
2017-08-18 22:32:20 -0400

    Adding stars to all positions, using playerid for selector

--- tiersheet/static/js/scripts.js
+++ tiersheet/static/js/scripts.js
@@ -3,21 +3,4 @@ $(function() {
     $(this).toggleClass('highlighted');
     });
 });
-$(document).on("click","#toggleStar", function()   {  
-        var player_id = $(this).attr("data-playerid");
-        $.ajax
-        ({ 
-            type: "POST",
-            url: "{% url 'toggleStar' %}", 
-            data: {'player_id': player_id, 'csrfmiddlewaretoken': '{{csrf_token}}'},
-            success: function(data){
-                // if ($(this).hasClass("glyphicon.glyphicon-star")) {
-                    $(this).find('i').toggleClass("glyphicon-star glyphicon-star-empty");
-                    // $(this).removeClass("glyphicon.glyphicon-star").addClass("glyphicon.glyphicon-star-empty");
-                //} else {
-                    //  $(this).removeClass("glyphicon.glyphicon-star-empty").addClass("glyphicon.glyphicon-star");
-                // }
-                //$this.toggleClass('glyphicon-star-empty glyphicon-star'); 
-                }
-            });
-        });
+
